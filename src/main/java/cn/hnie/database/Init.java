82a967b1f9d75986a14967a1adad5a3e1848e40e
conley <1215097846@qq.com>
2022-12-22 22:04:59 +0800

    test

---
+++ src/main/java/cn/hnie/database/Init.java
@@ -0,0 +1,10 @@
+package cn.hnie.database;
+
+import javax.sql.DataSource;
+
+public class Init {
+    private static DataSource dataSource;
+    static {
+        System.out.println("test");
+    }
+}
---
+++ src/test/java/cn/hnie/database/InitTest.java
@@ -0,0 +1,4 @@
+package cn.hnie.database;
+
+public class InitTest {
+}
